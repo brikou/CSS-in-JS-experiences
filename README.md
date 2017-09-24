58ff2b11e43fcd0e1c730da429a2fa5812341905
Brikou Carré <brikou@gmail.com>
2017-09-24 13:59:51 +0200

    Add some links

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 CSS-in-JS experiences
 =====================
+
+Hosted experiences can be found [here](https://css-in-js-experiences.ga/).
--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,11 @@ export default () => (
         </h1>
         <p>
             Original example can be found{" "}
-            <a target="_blank" href="http://getbootstrap.com/docs/4.0/examples/jumbotron/">
+            <a href="http://getbootstrap.com/docs/4.0/examples/jumbotron/">
                 here
-            </a>.
+            </a>.<br />
+            Github repository is{" "}
+            <a href="https://github.com/brikou/CSS-in-JS-experiences">here</a>.
         </p>
     </div>
 );
