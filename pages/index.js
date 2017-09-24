a1a93bf645cb7a7c12bfa53cf77e55779719e358
Brikou Carré <brikou@gmail.com>
2017-09-24 12:48:38 +0200

    Add index page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default () => (
+    <div style={{ padding: "2em" }}>
+        <ul>
+            <li>
+                <Link href="/bootstrap">
+                    <a>Integration of Bootstrap with Emotion</a>
+                </Link>
+            </li>
+        </ul>
+    </div>
+);
